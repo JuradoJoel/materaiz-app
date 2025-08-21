b1b075be01f7c1f6b8acd3a7108d952e634c1659
Jurado Joel <S.JoelJurado@hotmail.com>
2025-08-21 16:29:31 -0300

    cambio nombre del mock, precio pasado a numeros y nuevo componente para productos

---
+++ src/components/product/types.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  id: number;
+  name: string;
+  original_Price: number;
+  discount_Price: number;
+  image: string;
+}
---
+++ src/features/home/ProductCard.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, Card, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
+import RemoveIcon from '@mui/icons-material/Remove';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AddIcon from '@mui/icons-material/Add';
+import { Product } from 'src/components/product/types';
+
+interface ProductCardProps {
+  product: Product;
+}
+
+function ProductCard({ product }: ProductCardProps) {
+  const formatCurrency = (value: number) =>
+    new Intl.NumberFormat('es-AR', {
+      style: 'currency',
+      currency: 'ARS',
+      minimumFractionDigits: 0,
+    }).format(value);
+  return (
+    <>
+      <Card
+        key={product.id}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          p: 1,
+          mb: 1,
+          borderBottom: '1px solid #eee',
+        }}
+      >
+        <CardMedia
+          component="img"
+          image={product.image}
+          alt={product.name}
+          sx={{ width: '80px', height: '80px', mr: 2 }}
+        />
+        <CardContent sx={{ flexGrow: 1, p: 0 }}>
+          <Typography variant="body1">{product.name}</Typography>
+          <Typography variant="body2">
+            <Box
+              component="span"
+              sx={{ textDecoration: 'line-through', color: 'neutral.main', marginRight: 1 }}
+            >
+              {formatCurrency(product.original_Price)}
+            </Box>
+            <Box component="span" sx={{ color: 'error.main', fontWeight: 'bold' }}>
+              {formatCurrency(product.discount_Price)}
+            </Box>
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: 'flex', alignItems: 'center', mr: 1 }}>
+          <IconButton sx={{ bgcolor: 'grey.300' }}>
+            <RemoveIcon />
+          </IconButton>
+          <Typography sx={{ mx: 1 }}>1</Typography>
+          <IconButton sx={{ bgcolor: 'grey.300' }}>
+            <AddIcon />
+          </IconButton>
+        </Box>
+        <Button
+          variant="contained"
+          sx={{ bgcolor: 'secondary.main', '&:hover': { bgcolor: 'secondary.dark' } }}
+        >
+          <ShoppingCartIcon />
+        </Button>
+      </Card>
+      )
+    </>
+  );
+}
+
+export default ProductCard;
