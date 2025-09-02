6b214e98558dd79532a449227361ee46521346b4
Jurado Joel <S.JoelJurado@hotmail.com>
2025-09-02 15:18:54 -0300

    cambios en la vista mobile del carrito de compras, se ajusta el nombre del producto y se alinea el contenido de la card

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -39,7 +39,93 @@ const Cart = () => {
 
           {cartProducts.map((item, index) => (
             <Card key={index} sx={{ mb: 2, p: 2 }}>
-              <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+              {/* mobile */}
+              <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={4}>
+                    <img
+                      src={item.product.image}
+                      alt={item.product.name}
+                      style={{
+                        width: '100%',
+                        aspectRatio: 1,
+                        objectFit: 'cover',
+                        borderRadius: 10,
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        fontWeight: 500,
+                        mb: 1,
+                        textTransform: 'none',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        whiteSpace: 'nowrap',
+                      }}
+                    >
+                      {item.product.name}
+                    </Typography>
+                    <Typography
+                      color="error"
+                      variant="body2"
+                      sx={{ cursor: 'pointer', mb: 1 }}
+                      onClick={() => {}}
+                    >
+                      Eliminar
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          gap: 1,
+                        }}
+                      >
+                        <IconButton
+                          size="small"
+                          sx={{
+                            bgcolor: 'grey.200',
+                          }}
+                        >
+                          <RemoveIcon fontSize="small" />
+                        </IconButton>
+                        <Typography sx={{ minWidth: 15, textAlign: 'center' }}>
+                          {item.quantity}
+                        </Typography>
+                        <IconButton
+                          size="small"
+                          sx={{
+                            bgcolor: 'grey.200',
+                          }}
+                        >
+                          <AddIcon fontSize="small" />
+                        </IconButton>
+                      </Box>
+                      <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                        ${item.product.original_price.toLocaleString()}
+                      </Typography>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+
+              {/* desktop */}
+              <Box
+                sx={{
+                  display: { xs: 'none', md: 'flex' },
+                  alignItems: 'center',
+                  gap: 2,
+                }}
+              >
                 <img
                   src={item.product.image}
                   alt={item.product.name}
